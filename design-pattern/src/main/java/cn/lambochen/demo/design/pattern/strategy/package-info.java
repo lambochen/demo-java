798b6ada43f84c138bf0376998eaf646d79f0625
lambochen <lambo.chen.2306@gmail.com>
2022-04-05 22:24:19 +0800

    策略模式

--- design-pattern/src/main/java/cn/lambochen/demo/design/pattern/strategy/package-info.java
+++ design-pattern/src/main/java/cn/lambochen/demo/design/pattern/strategy/package-info.java
@@ -1,6 +1,11 @@
 package cn.lambochen.demo.design.pattern.strategy;
 /**
- * @Author lambo.chen.2306@gmail.com
- * @Date 2020/7/16 21:28
- * @Description 策略模式
+ * 策略模式
+ *
+ * Strategy（策略接口）：
+ *      定义通用的策略规范标准，包含在系统环境中并声明策略接口标准。
+ * ConcreteStrategy（策略实现）：
+ *      实现了策略接口的策略实现类，可以有多种不同的策略实现，但都得符合策略接口定义的规范。
+ * Context（系统环境）：
+ *      包含策略接口的系统环境，对外提供更换策略实现的方法 setStrategy() 以及执行策略的方法 execute()，其本身并不关心执行的是哪种策略实现。
  **/
