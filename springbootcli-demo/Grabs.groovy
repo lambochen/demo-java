ffb1acdb721f6c0f2b99d8cbad279ebf81df07ce
lambochen <lambo.chen.2306@gmail.com>
2022-06-25 16:53:48 +0800

    actuator

--- springbootcli-demo/Grabs.groovy
+++ springbootcli-demo/Grabs.groovy
@@ -1,3 +1,9 @@
+// h2 内存数据库
 @Grab("h2")
+// thymeleaf 模版引擎
 @Grab("spring-boot-starter-thymeleaf")
+// Spring Boot Actuator
+@Grab("spring-boot-starter-actuator")
+// 远程shell，用于进入应用程序
+//@Grab("spring-boot-starter-remote-shell")
 class Grabs {}
