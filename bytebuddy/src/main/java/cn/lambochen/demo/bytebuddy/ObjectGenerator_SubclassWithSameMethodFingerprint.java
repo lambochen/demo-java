172f8a8d6b481774a0109b4ac8400320f9f80a05
lambochen <lambo.chen.2306@gmail.com>
2022-07-28 00:22:32 +0800

    ObjectGenerator_SubclassWithSameMethodFingerprint

---
+++ bytebuddy/src/main/java/cn/lambochen/demo/bytebuddy/ObjectGenerator_SubclassWithSameMethodFingerprint.java
@@ -0,0 +1,71 @@
+package cn.lambochen.demo.bytebuddy;
+
+import com.sun.tools.javah.Gen;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
+
+/**
+ * @author lambochen
+ * @date 2022-07-28 00:12
+ */
+public class ObjectGenerator_SubclassWithSameMethodFingerprint {
+
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+        Class<? extends People> clazz = generateSubclassWithSameMethodFingerprint();
+        People people = clazz.newInstance();
+
+        System.out.println(clazz.getName());
+        System.out.println(people.say());
+
+        /**
+         * 输出：
+         *
+         * cn.lambochen.demo.bytebuddy.SubclassWithSameMethodFingerprint
+         * 你好，ByteBuddy
+         */
+    }
+
+    /**
+     * 将拦截后到方法具体逻辑，委派到其它类执行
+     *
+     * 几点要求：
+     * 1、People接口和Chinese类必须是公共的可访问的
+     * 2、Chinese 的方法除了名字外，其它定义必须同 People
+     * 3、该方式仅支持 Chinese 中只有一个相同签名的函数，否则会报错
+     *
+     *
+     * @return
+     */
+    static Class<? extends People> generateSubclassWithSameMethodFingerprint() {
+        DynamicType.Unloaded<People> unloaded = new ByteBuddy()
+                .subclass(People.class)
+                .name("cn.lambochen.demo.bytebuddy.SubclassWithSameMethodFingerprint")
+                // 此处指明了需要拦截的方法
+                .method(ElementMatchers.named("say"))
+                // 委派到其它类实现
+                .intercept(MethodDelegation.to(new Chinese()))
+                .make();
+
+        return unloaded.load(ObjectGenerator_SubclassWithSameMethodFingerprint.class.getClassLoader()).getLoaded();
+    }
+
+    public interface People {
+        String say();
+    }
+
+    public static class Chinese {
+        /**
+         * 签名（除方法名外）必须和 People 的相同
+         * @return
+         */
+        public String sayChinese() {
+            return "你好，ByteBuddy";
+        }
+
+//        public String sayChinese1() {
+//            return "你好，ByteBuddy";
+//        }
+    }
+}
