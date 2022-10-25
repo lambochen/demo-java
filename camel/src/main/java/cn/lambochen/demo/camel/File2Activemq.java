748d3d4d52ca404c58fdbf848e97415d38683eb9
lambochen <lambo.chen.2306@gmail.com>
2022-10-25 19:19:20 +0800

    pub sub

--- camel/src/main/java/cn/lambochen/demo/camel/File2Activemq.java
+++ camel/src/main/java/cn/lambochen/demo/camel/File2Activemq.java
@@ -1,12 +1,5 @@
 package cn.lambochen.demo.camel;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.camel.CamelContext;
-import org.apache.camel.Component;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.jms.JmsComponent;
-import org.apache.camel.impl.DefaultCamelContext;
-
 /**
  * @author lambochen
  * @date 2022-10-25 17:54
@@ -14,22 +7,22 @@ import org.apache.camel.impl.DefaultCamelContext;
 public class File2Activemq {
 
     public static void main(String[] args) throws Exception {
-        CamelContext camelContext = new DefaultCamelContext();
-
-        ActiveMQConnectionFactory mqConnectionFactory = new ActiveMQConnectionFactory(
-                "admin", "admin", "http://127.0.0.1:8161"
-        );
-        camelContext.addComponent("activemq", (Component) JmsComponent.jmsComponentAutoAcknowledge(mqConnectionFactory));
-
-        camelContext.addRoutes(new RouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                from("file:input_box")
-                        .to("activemq:queue:myqueue");
-            }
-        });
-
-        camelContext.start();
+//        CamelContext camelContext = new DefaultCamelContext();
+//
+//        ActiveMQConnectionFactory mqConnectionFactory = new ActiveMQConnectionFactory(
+//                "admin", "admin", "http://127.0.0.1:8161"
+//        );
+//        camelContext.addComponent("activemq", (Component) JmsComponent.jmsComponentAutoAcknowledge(mqConnectionFactory));
+//
+//        camelContext.addRoutes(new RouteBuilder() {
+//            @Override
+//            public void configure() throws Exception {
+//                from("file:input_box")
+//                        .to("activemq:queue:myqueue");
+//            }
+//        });
+//
+//        camelContext.start();
     }
 
 }
---
+++ camel/src/main/java/cn/lambochen/demo/camel/PubSub.java
@@ -0,0 +1,52 @@
+package cn.lambochen.demo.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ConsumerTemplate;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ * @author lambochen
+ * @date 2022-10-25 19:09
+ */
+public class PubSub {
+
+    public static void main(String[] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                        .process(new Processor() {
+                            @Override
+                            public void process(Exchange exchange) throws Exception {
+                                System.out.println("消息处理...start");
+
+                                String message = exchange.getIn().getBody(String.class);
+
+                                message = message + "-process";
+
+                                // repub
+                                exchange.getOut().setBody(message);
+                            }
+                        })
+                        .to("seda:end");
+            }
+        });
+        context.start();
+
+        // produce
+        ProducerTemplate producerTemplate = context.createProducerTemplate();
+        producerTemplate.sendBody("direct:start", "originalMsg");
+
+        // consume
+        ConsumerTemplate consumerTemplate = context.createConsumerTemplate();
+        String message = consumerTemplate.receiveBody("seda:end", String.class);
+
+        System.out.println("consume msg body: " + message);
+    }
+}
