2062608ef1b146603b987bafcd55a7b262cdb34b
lambochen <lambo.chen.2306@gmail.com>
2023-04-07 10:19:31 +0800

    cmt

--- springboot-ext/src/main/java/cn/lambochen/demo/springboot/ext/LogApplicationContextInitializer.java
+++ springboot-ext/src/main/java/cn/lambochen/demo/springboot/ext/LogApplicationContextInitializer.java
@@ -8,6 +8,11 @@ import org.springframework.context.ConfigurableApplicationContext;
  *
  * 时机 : spring 容器还没被刷新之前准备阶段 this.prepareContext(context, environment, listeners, applicationArguments, printedBanner);
  * 此时所有的配置文件都已经加载
+ *
+ * 这时候spring容器还没被初始化，所以想要自己的扩展的生效，有以下三种方式：
+ * 在启动类中用springApplication.addInitializers(new TestApplicationContextInitializer())语句加入
+ * 配置文件配置context.initializer.classes=com.example.demo.TestApplicationContextInitializer
+ * Spring SPI扩展，在spring.factories中加入org.springframework.context.ApplicationContextInitializer=com.example.demo.TestApplicationContextInitializer
  */
 public class LogApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
     @Override
--- springboot-ext/src/main/java/cn/lambochen/demo/springboot/ext/package-info.java
+++ springboot-ext/src/main/java/cn/lambochen/demo/springboot/ext/package-info.java
@@ -3,4 +3,5 @@ package cn.lambochen.demo.springboot.ext;
  * @author lambochen
  *
  * https://juejin.cn/post/6999603401239576583
+ * https://segmentfault.com/a/1190000023033670
  */
